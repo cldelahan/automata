c1aaa67ecc2624d9feecba32ce53a41bb9bd9540
cldelahan <cldelahan@gmail.com>
2018-12-21 23:32:01 -0500

    added comparable implementation

---
+++ DFA.java
@@ -0,0 +1,20 @@
+// Conner Delahanty
+// DFA represents a graph in its formal format
+import java.util.Graph;
+
+public class DFA {
+
+    private ArrayList<State> Q;
+    private ArrayList<Char> sigma;
+    private State startState;
+    private ArrayList<State> acceptStates;
+
+    // should I put State and Transition as inner classes?
+    
+
+    public DFA(){}
+
+    public DFA();
+
+
+}
